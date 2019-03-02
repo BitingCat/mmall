b95d5b50f7479e1f8e463dcddc30adadd7bb242e
wen <1057656161@qq.com>
2019-03-02 18:42:38 +0800

    third commit init project

---
+++ src/main/java/com/mmall/common/ServerResponse.java
@@ -0,0 +1,91 @@
+package com.mmall.common;
+
+import net.sf.jsqlparser.schema.Server;
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.io.Serializable;
+
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+//保证序列化json的时候，如果是null的对象，key也会消失
+public class ServerResponse<T> implements Serializable {
+    private int status;
+    private String msg;
+    private T data;
+    //使用T，可以不指定返回的类型，可根据不同的请求返回不同响应的内容
+
+    //上面的注解表示，当调用这个构造器时，只需要返回status，而msg,data就不需要返回了
+    private ServerResponse(int status) {
+        this.status = status;
+    }
+
+    private ServerResponse(int status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    private ServerResponse(int status, String msg, T data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    private ServerResponse(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    //添加注解，防止这个函数返回值一起序列化为Json格式返回给页面
+    //使之不在json序列化结果当中
+    @JsonIgnore
+    public boolean isSuccess() {
+        return this.status == ResponseCode.SUCCESS.getCode();
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public static <T> ServerResponse<T> createBySuccess() {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+    }
+
+    public static <T> ServerResponse<T> createBySuccessMessage(String msg) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg);
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(T data) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), data);
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(String msg, T data) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg, data);
+    }
+
+    public static <T> ServerResponse<T> createByError() {
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(), ResponseCode.ERROR.getDesc());
+    }
+
+    public static <T> ServerResponse<T> createByErrorMessage(String errorMessage) {
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(), errorMessage);
+    }
+
+    public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode, String errorMessage) {
+        return new ServerResponse<T>(errorCode, errorMessage);
+    }
+
+//    当为public时，T为String类型时，两个构造方法可能会重复，通过对外public方法问题规避这个问题
+//    public static void main(String[] args) {
+//        ServerResponse sr1 = new ServerResponse(1, new Object());
+//        ServerResponse sr2 = new ServerResponse(1, "abc");
+//        System.out.println("consle");
+//    }
+}
